bc29dae694b43a11f22695d14d8865bbcb290fe4
mohelhaj <42706914+mohelhaj@users.noreply.github.com>
2018-08-28 13:31:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,19 +28,6 @@ client.user.setGame(`Streming`,"Manager")
   console.log('')
 });
 
-const discord = require("discord.js");
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
 client.on('ready',  () => {
   console.log('By : Mr.LOVE ');
   console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
